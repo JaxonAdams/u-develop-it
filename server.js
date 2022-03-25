635415f8279f696019e84c82fc80bc70837ad1be
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-25 12:55:25 -0600

    plugged in database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 // Define PORT
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to MySQL database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // MySQL username
+        user: "root",
+        // MySQL password
+        password: 'PLAjuly25!',
+        database: 'election'
+    }, 
+    console.log('Connected to the database.')
+);
+
+// return data in candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
